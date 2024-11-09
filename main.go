d3ccbd2cdf02935f85c03627c9676d38c4377d3f
Chhin Hua <chhinhua@gmail.com>
2024-11-09 16:40:23 +0700

    fet: create new item

--- main.go
+++ main.go
@@ -20,6 +20,17 @@ type TodoItem struct {
 	UpdatedAt   *time.Time `json:"updated_at,omitempty"`
 }
 
+type TodoItemCreation struct {
+	Id          int    `json:"-" gorm:"column:id"`
+	Title       string `json:"title" gorm:"column:title"`
+	Description string `json:"description" gorm:"column:description"`
+	Status      string `json:"status" gorm:"column:status"`
+}
+
+func (TodoItemCreation) TableName() string {
+	return "todo_items"
+}
+
 func main() {
 	dsn := os.Getenv("DB_CONN_STR")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -43,10 +54,53 @@ func main() {
 	}
 
 	r := gin.Default()
+	//	CRUD: Create, Read, Update, Delete
+	//	POST /v1/items (create a new item)
+	//	GET /v1/items (list items) /v1/items?page=1
+	//	GET /v1/items/:id (get item detail by id)
+	//	(PUT || PATCH) /v1/items/:id (update an item by id)
+	//	DELETE /v1/items/:id (delete an item by id)
+
+	v1 := r.Group("/v1")
+	{
+		items := v1.Group("/items")
+		{
+			items.POST("", CreateItem(db))
+			items.GET("")
+			items.GET("/:id")
+			items.PATCH("/:id")
+			items.DELETE("/:id")
+		}
+	}
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": item,
 		})
 	})
 	_ = r.Run(":8081") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func CreateItem(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
+		var data TodoItemCreation
+
+		if err := c.ShouldBind(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+
+			return
+		}
+
+		if err := db.Create(&data).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+		}
+
+		c.JSON(http.StatusCreated, gin.H{
+			"data": data.Id,
+		})
+	}
 }
